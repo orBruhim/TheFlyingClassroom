34a2e43bcd14954ca71376040dc0609e73fa77de
EllaSharakanski <Ella10004@gmail.com>
2022-03-08 14:30:47 +0200

    zoom and cors

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express'
+import cors from 'cors'
 import { getClasses } from './service/class.service'
 const app = express()
+app.use(cors())
 const port = 5000
 
 app.get('/api/v1/classes', (_, res) => {
