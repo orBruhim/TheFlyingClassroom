921f7b3cf17b05564deddb2520651804a805100d
Mor Schenavsky <m.schenavsky@gmail.com>
2022-03-08 14:03:40 +0200

    added Monday classes

--- Backend/src/data/classes.ts
+++ Backend/src/data/classes.ts
@@ -37,6 +37,42 @@ export const classList: Class[] =
     "endTime": "12:00",
     "link": "http://zoom.com"
     },
+
+    {
+    "id": 5,
+    "name": "English",
+    "day": 2,
+    "startTime": "08:00",
+    "endTime": "09:00",
+    "link": "http://zoom.com"
+    },
+    
+    {
+    "id": 6,
+    "name": "Hebrew",
+    "day": 2,
+    "startTime": "09:00",
+    "endTime": "10:00",
+    "link": "http://zoom.com"
+    },
+    
+    {
+    "id": 7,
+    "name": "Math",
+    "day": 2,
+    "startTime": "10:00",
+    "endTime": "11:00",
+    "link": "http://zoom.com"
+    },
+    
+    {
+    "id": 8,
+    "name": "Hebrew",
+    "day": 2,
+    "startTime": "11:00",
+    "endTime": "12:00",
+    "link": "http://zoom.com"
+    },
     
 
 
