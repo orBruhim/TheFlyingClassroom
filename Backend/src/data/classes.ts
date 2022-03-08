06ba920bcac7c4c634e563d2d571c808c17f88fc
Mor Schenavsky <m.schenavsky@gmail.com>
2022-03-08 13:38:27 +0200

    added Sunday classes

--- Backend/src/data/classes.ts
+++ Backend/src/data/classes.ts
@@ -11,5 +11,33 @@ export const classList: Class[] =
     "link": "http://zoom.com"
     },
 
+    {
+    "id": 2,
+    "name": "Math",
+    "day": 1,
+    "startTime": "09:00",
+    "endTime": "10:00",
+    "link": "http://zoom.com"
+    },
+
+    {
+    "id": 3,
+    "name": "Science",
+    "day": 1,
+    "startTime": "10:00",
+    "endTime": "11:00",
+    "link": "http://zoom.com"
+    },
+
+    {
+    "id": 4,
+    "name": "Sport",
+    "day": 1,
+    "startTime": "11:00",
+    "endTime": "12:00",
+    "link": "http://zoom.com"
+    },
+    
+
 
 ]
